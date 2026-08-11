de8eb96a9fb488c8b53f6a4c82d03e16ca85ff56
7w1 <noreply@sl.sable.moe>
2026-08-11 00:08:20 -0500

    Revert "add missing file"

--- src/lib/core/context.ts
+++ src/lib/core/context.ts
@@ -1,15 +1,5 @@
-import { getContext, setContext } from 'svelte';
+import { createContext } from 'svelte';
 
 import type { CoreClient } from './client.svelte';
 
-const CORE_CLIENT = Symbol('core-client');
-
-export function provideCoreClient(client: CoreClient): void {
-	setContext(CORE_CLIENT, client);
-}
-
-export function useCoreClient(): CoreClient {
-	const client = getContext<CoreClient>(CORE_CLIENT);
-	if (!client) throw new Error('Sable core has not been provided');
-	return client;
-}
+export const [useCoreClient, provideCoreClient] = createContext<CoreClient>();
