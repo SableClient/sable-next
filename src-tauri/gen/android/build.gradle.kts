b7ed3fbab8ae00dcc3515b21480f746256f50519
Erwan Leboucher <erwanleboucher@gmail.com>
2026-08-18 10:11:26 +0200

    fix: build the android plugins with kotlin 2.1

--- src-tauri/gen/android/build.gradle.kts
+++ src-tauri/gen/android/build.gradle.kts
@@ -5,7 +5,9 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.11.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.25")
+        // Above the Tauri template's 1.9.25: the UnifiedPush connector the
+        // notifications plugin pulls in ships Kotlin 2.1 metadata.
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.21")
     }
 }
 
