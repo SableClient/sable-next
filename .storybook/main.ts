7655b6a4be13576b33fab10dec97900e8928622d
Erwan Leboucher <erwanleboucher@gmail.com>
2026-08-25 22:45:20 +0200

    fix storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,7 @@ import type { StorybookConfig } from '@storybook/sveltekit';
 
 const config: StorybookConfig = {
   framework: '@storybook/sveltekit',
-  stories: ['../src/lib/ui*.stories.svelte'],
+  stories: ['../src/lib/ui/**/*.stories.svelte'],
   addons: ['@storybook/addon-svelte-csf', '@storybook/addon-docs', '@storybook/addon-a11y'],
   staticDirs: ['../static'],
 };
