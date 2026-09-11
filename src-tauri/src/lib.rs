5f1d0e2de81faf5e7e94387c45b95eca3d150484
Erwan Leboucher <erwanleboucher@gmail.com>
2026-09-11 15:20:17 +0200

    chore: gate dead code with knip and cargo-machete
    Signed-off-by: Erwan Leboucher <erwanleboucher@gmail.com>

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -476,7 +476,6 @@ pub fn run() {
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_os::init())
-        .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_notifications::init())
         .setup(setup)
         .invoke_handler(tauri::generate_handler![
